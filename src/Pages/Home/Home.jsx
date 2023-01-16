c14848207658f8994e3b2eaace5189c2b6eb2416
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-16 20:23:09 +0600

    Show Login form in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Dashboard from '../Dashboard/Dashboard/Dashboard';
+import Login from '../LoginsAndRegisters/Login/Login';
 
 const Home = () => {
   return (
     <div>
-
+      <Login></Login>
     </div>
   );
 };
