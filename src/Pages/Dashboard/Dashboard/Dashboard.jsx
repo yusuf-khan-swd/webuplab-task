9e7e9d469c50fd58593f7f5547f3c3d766e6b7f4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-16 18:57:39 +0600

    modified style

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
 
   return (
     <div className="mt-4">
-      <h2 className="text-xl text-center">Welcome <span className="text-blue-600 font-bold">{user?.displayName}</span></h2>
+      <h2 className="text-xl text-center lg:text-left lg:ml-12">Welcome <span className="text-blue-600 font-bold">{user?.displayName}</span></h2>
     </div>
   );
 };
