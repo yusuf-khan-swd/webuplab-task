6e3b4f67501ca2a9c19bc902353569f0fcbaa371
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-16 18:50:53 +0600

    Create dashboard layout route

--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayouts from "../../Layouts/DashboardLayouts";
 import Parent from "../../Layouts/Parent";
+import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import Data from "../../Pages/Dashboard/Data/Data";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/LoginsAndRegisters/Login/Login";
 import About from "../../Pages/Other/About/About";
@@ -27,7 +30,22 @@ export const router = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <PrivateRoute><About></About></PrivateRoute>
+        element: <About></About>
+      },
+    ]
+  },
+  {
+    path: "/dashboard",
+    element: <PrivateRoute><DashboardLayouts></DashboardLayouts></PrivateRoute>,
+    errorElement: <DisplayError></DisplayError>,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>
+      },
+      {
+        path: "/dashboard/data",
+        element: <Data></Data>
       }
     ]
   }
